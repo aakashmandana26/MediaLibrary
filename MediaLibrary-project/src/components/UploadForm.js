1d488b5402ffa9958fb917079e07d101af47f347
Akash Mandana <aakash.mandana26@gmail.com>
2022-11-04 22:43:40 +0530

    first commit

---
+++ MediaLibrary-project/src/components/UploadForm.js
@@ -0,0 +1,79 @@
+import React, { useState } from "react";
+import axios from "axios";
+import { BACKEND_URI } from "../config/constants";
+
+const UploadForm = ({ getAllMedias }) => {
+  const [name, setName] = useState("");
+  const [description, setDescription] = useState("");
+  const [videos, setVideos] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    let formdata = new FormData();
+    for (let key in videos) {
+      formdata.append("videos", videos[key]);
+    }
+
+    formdata.append("name", name);
+    formdata.append("description", description);
+
+    axios
+      .post(`${BACKEND_URI}/api/v1/media/create`, formdata)
+      .then((success) => {
+        getAllMedias();
+        alert("Submitted successfully");
+      })
+      .catch((error) => {
+        console.log(error);
+        alert("Error happened!");
+      });
+  };
+
+  return (
+    <>
+      <form onSubmit={handleSubmit}>
+        <div className="form-group">
+          <label htmlFor="name">Title</label>
+          <input
+            type="text"
+            name="name"
+            id="name"
+            className="form-control"
+            onChange={(e) => setName(e.target.value)}
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="name">Description</label>
+          <input
+            type="text"
+            name="description"
+            id="description"
+            className="form-control"
+            onChange={(e) => setDescription(e.target.value)}
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="videos">Upload Videos</label>
+          <input
+            type="file"
+            name="videos"
+            id="videos"
+            multiple
+            className="form-control"
+            accept=".mp4, .mkv"
+            onChange={(e) => {
+              setVideos(e.target.files);
+            }}
+          />
+        </div>
+
+        <button type="submit" className="btn btn-primary mt-2">
+          Submit
+        </button>
+      </form>
+    </>
+  );
+};
+
+export default UploadForm;
